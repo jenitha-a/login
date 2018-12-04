481a0e4934844b9b799a7d8278669b8b201867e7
imvjenitha <jenitha.abraham@gmail.com>
2018-12-04 17:47:40 +0530

    first commit

---
+++ src/app/user.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface myData {
+  message: string,
+  success: boolean
+}
+
+interface isLoggedIn {
+  status: boolean
+}
+
+interface logoutStatus {
+  success: boolean
+}
+@Injectable()
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  getSomeData() {
+    return this.http.get<myData>('assets/php/database.php')
+  }
+
+  isLoggedIn(): Observable<isLoggedIn> {
+    return this.http.get<isLoggedIn>('assets/php/isloggedin.php')
+  }
+
+  logout() {
+    return this.http.get<logoutStatus>('assets/php/logout.php')
+  }
+
+}
